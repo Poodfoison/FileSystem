fc5d0b3b8c848255f520389b302776f5a98768cf
Patrick Henry <108245467+Poodfoison@users.noreply.github.com>
2022-08-22 16:33:58 +0800

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,1 +1,62 @@
+const http = require('http');
+const fs = require('fs/promises')
+const PORT = 3000
+ 
 
+const requestListener = async (req, res) => {
+    let file;
+    if (req.url ==='/home' || req.url === '/') {
+    try {
+         file = await fs.readFile("./routes/home.html")
+       
+        
+    } catch (error) {
+        res.writeHead(500)
+        res.end(file)
+    }
+}
+    else if (req.url ==='/about' ){
+        try {
+             file = await fs.readFile("./routes/about.html")
+            
+        } catch (error) {
+            res.writeHead(500)
+            res.end(file)
+        }
+    }
+    else if (req.url ==='/contacts' ){
+        try {
+             file = await fs.readFile("./routes/contacts.html")
+            
+        } catch (error) {
+            res.writeHead(500)
+            res.end(file)
+        }
+    }
+    else {
+        try {
+            file = await fs.readFile("./routes/404.html")
+           
+       } catch (error) {
+           res.writeHead(500)
+           res.end(file)
+       }
+
+    }
+
+
+    console.log(file )
+    res.setHeader("Content-Type", "text/html")
+    res.writeHead(200)
+    res.end(file)
+}
+
+const server = new http.Server(requestListener)
+
+server.listen(PORT, (err) => {
+    if (err){
+        console.log(err)
+        throw err;
+    }
+    console.log(`Server has started on http://localhost:${PORT}!`)
+})
