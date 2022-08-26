edfe8927f63ea524d6eb4e704a991d9be74e9ade
Patrick Henry <108245467+Poodfoison@users.noreply.github.com>
2022-08-26 09:51:15 +0800

    Add files via upload

--- Debugging/Problem5/BetterModule.js
+++ Debugging/Problem5/BetterModule.js
@@ -1,1 +1,49 @@
+const operation_4 = (secondVal) => {
+    let res = 0;
+    while(secondVal-res > 1){
+        secondVal -= 1;
+        res += 1;
+    }
+    return res;
+}
 
+
+const operation_3 = (firstVal, thirdVal) => {
+    let rep = 0;
+    for(let i=1; i<=firstVal; i++){
+        rep += firstVal;
+    }
+    rep = operation_2(rep, thirdVal);
+    return rep;
+}
+
+const operation_2 = (firstVal, thirdVal) => {
+    let ret = 0;
+    let copy = firstVal;
+    while(firstVal >= thirdVal) {
+        firstVal -= thirdVal;
+        ret += 1;
+    }
+    return copy - ret*thirdVal;
+}
+
+const operation_1 = (firstVal, secondVal, thirdVal) => {
+    if(secondVal == 0) {
+        return 1;
+    } else if(secondVal == 1) {    
+        return operation_2(firstVal, thirdVal);
+    } else {
+        let fourthVal = operation_1(operation_3(firstVal, thirdVal), operation_4(secondVal), thirdVal);
+    
+    	if(secondVal%2 == 1) {
+            return operation_2(firstVal*fourthVal, thirdVal);
+        } else {
+            return fourthVal;
+        }
+    }
+};
+
+
+console.log(operation_1(3,2,4)) // prints 1
+console.log(operation_1(4,3,2)) // prints 0
+console.log(operation_1(9,6,19)) // prints 11
